ede37d1315383744428a1b10c87faffc11edc863
pike <pike@SilverAir>
2021-06-25 23:32:38 +0200

    update 202106

---
+++ reference/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"nl.kw.processing.ExampleBazPackage","l":"ExampleBar"},{"p":"nl.kw.processing.ExampleBazPackage","l":"ExampleQuz"}]
